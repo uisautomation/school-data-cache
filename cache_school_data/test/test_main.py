fe537168b1ce8a5e20242c2c2c90bcfbc0019d41
Mike Bamford <mb2174@cam.ac.uk>
2017-10-24 15:09:10 +0100

    simplified test

--- cache_school_data/test/test_main.py
+++ cache_school_data/test/test_main.py
@@ -9,7 +9,7 @@ from cache_school_data.settings_ansible import SCHOOL_DATA_CACHE, AWS_STORAGE_BU
 
 class Capture:
     """
-    FIXME
+    Captures an argument value from a call on a mock.
     """
     value = None
 
@@ -21,7 +21,10 @@ class Capture:
 class UtilsTests(TestCase):
 
     def test_cache_school_data(self):
+
         mocked_api = Mock()
+        mocked_api.bind.return_value = mocked_api
+
         mocked_api.GetSchoolsJSON.return_value = """
             [{"SchoolName": "Non-School", "SchoolCode": "O"},
              {"SchoolName": "School of Arts and Humanities", "SchoolCode": "A"},
@@ -54,71 +57,36 @@ class UtilsTests(TestCase):
             return "[]"
 
         mocked_api.GetDepartmentsJSON.side_effect = side_effect
-        self.patch_soap_client('cache_school_data.main.contacts_client', mocked_api)
 
-        mock = MagicMock()
+        with patch("cache_school_data.main.contacts_client") as contacts_client:
+            contacts_client.return_value = mocked_api
 
-        def side_effect(*args, **kwargs):
-            return mock
+            with patch("cache_school_data.main.s3_client") as s3_client:
+                mock = MagicMock()
+                s3_client.return_value = mock
 
-        self.patch("cache_school_data.main.s3_client", side_effect=side_effect)
+                # test
+                cache_school_data()
 
-        # test
-        cache_school_data()
+                body_capture = Capture()
 
-        body_capture = Capture()
+                mock.put_object.assert_called_with(
+                    Body=body_capture,
+                    Bucket=AWS_STORAGE_BUCKET_NAME,
+                    Key=SCHOOL_DATA_CACHE,
+                    ACL='public-read'
+                )
 
-        mock.put_object.assert_called_with(
-            Body=body_capture,
-            Bucket=AWS_STORAGE_BUCKET_NAME,
-            Key=SCHOOL_DATA_CACHE,
-            ACL='public-read'
-        )
-
-        result = json.loads(body_capture.value)
-        self.assertEqual(len(result), 7)
-        self.assertEqual(result[0]['SchoolCode'], 'O')
-        self.assertEqual(result[0]['SchoolName'], 'Non-School')
-        self.assertEqual(len(result[0]['departments']), 3)
-        self.assertEqual(result[0]['departments'][0]['DeptCode'], 'GHAG')
-        self.assertEqual(result[0]['departments'][0]['DeptName'], 'Anglo Saxon, Norse and Celtic')
-        self.assertEqual(result[0]['departments'][2]['DeptCode'], 'GUAG')
-        self.assertEqual(result[0]['departments'][2]['DeptName'], 'Asian and Middle Eastern Studies')
-        self.assertEqual(result[0]['SchoolName'], 'Non-School')
-        self.assertEqual(result[6]['SchoolCode'], 'P')
-        self.assertEqual(result[6]['SchoolName'], 'School of the Physical Sciences')
-        self.assertEqual(len(result[6]['departments']), 0)
-
-    def patch_soap_client(self, target, client_mock):
-        """
-        FIXME
-
-        Helper method for patching utils.soap_client(). Takes care of the client.bind() call.
-
-        :param target: the patch target
-        :param client_mock: a mock object used to mock responses and test interractions
-        """
-        def side_effect(*args, **kwargs):
-            def bind(*args, **kwargs):
-                return client_mock
-            client_mock.bind = bind
-            return client_mock
-
-        self.patch(target, side_effect)
-
-    def patch(self, target_name, side_effect=None):
-        """
-        FIXME
-
-        Patching helper method
-
-        :param target_name: name of target method to patch
-        :param side_effect: optional side_effect to set
-        :return: the patched target method
-        """
-        patcher = patch(target_name)
-        target = patcher.start()
-        self.addCleanup(patcher.stop)
-        if side_effect:
-            target.side_effect = side_effect
-        return target
+                result = json.loads(body_capture.value)
+                self.assertEqual(len(result), 7)
+                self.assertEqual(result[0]['SchoolCode'], 'O')
+                self.assertEqual(result[0]['SchoolName'], 'Non-School')
+                self.assertEqual(len(result[0]['departments']), 3)
+                self.assertEqual(result[0]['departments'][0]['DeptCode'], 'GHAG')
+                self.assertEqual(result[0]['departments'][0]['DeptName'], 'Anglo Saxon, Norse and Celtic')
+                self.assertEqual(result[0]['departments'][2]['DeptCode'], 'GUAG')
+                self.assertEqual(result[0]['departments'][2]['DeptName'], 'Asian and Middle Eastern Studies')
+                self.assertEqual(result[0]['SchoolName'], 'Non-School')
+                self.assertEqual(result[6]['SchoolCode'], 'P')
+                self.assertEqual(result[6]['SchoolName'], 'School of the Physical Sciences')
+                self.assertEqual(len(result[6]['departments']), 0)
