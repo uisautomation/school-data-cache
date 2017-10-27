1b2ce51e48f819db66372a5f2ed3eda3028beb5f
Mike Bamford <mb2174@cam.ac.uk>
2017-10-27 15:35:25 +0100

    using @patch for cleaner unit test

--- cache_school_data/test/test_main.py
+++ cache_school_data/test/test_main.py
@@ -20,7 +20,9 @@ class Capture:
 
 class UtilsTests(TestCase):
 
-    def test_cache_school_data(self):
+    @patch('cache_school_data.main.contacts_client')
+    @patch('cache_school_data.main.s3_client')
+    def test_cache_school_data(self, s3_client, contacts_client):
 
         mocked_api = Mock()
         mocked_api.bind.return_value = mocked_api
@@ -58,35 +60,33 @@ class UtilsTests(TestCase):
 
         mocked_api.GetDepartmentsJSON.side_effect = side_effect
 
-        with patch("cache_school_data.main.contacts_client") as contacts_client:
-            contacts_client.return_value = mocked_api
+        contacts_client.return_value = mocked_api
 
-            with patch("cache_school_data.main.s3_client") as s3_client:
-                mock = MagicMock()
-                s3_client.return_value = mock
+        mock_s3 = MagicMock()
+        s3_client.return_value = mock_s3
 
-                # test
-                cache_school_data()
+        # test
+        cache_school_data()
 
-                body_capture = Capture()
+        body_capture = Capture()
 
-                mock.put_object.assert_called_with(
-                    Body=body_capture,
-                    Bucket=AWS_STORAGE_BUCKET_NAME,
-                    Key=SCHOOL_DATA_CACHE,
-                    ACL='public-read'
-                )
+        mock_s3.put_object.assert_called_with(
+            Body=body_capture,
+            Bucket=AWS_STORAGE_BUCKET_NAME,
+            Key=SCHOOL_DATA_CACHE,
+            ACL='public-read'
+        )
 
-                result = json.loads(body_capture.value)
-                self.assertEqual(len(result), 7)
-                self.assertEqual(result[0]['SchoolCode'], 'O')
-                self.assertEqual(result[0]['SchoolName'], 'Non-School')
-                self.assertEqual(len(result[0]['departments']), 3)
-                self.assertEqual(result[0]['departments'][0]['DeptCode'], 'GHAG')
-                self.assertEqual(result[0]['departments'][0]['DeptName'], 'Anglo Saxon, Norse and Celtic')
-                self.assertEqual(result[0]['departments'][2]['DeptCode'], 'GUAG')
-                self.assertEqual(result[0]['departments'][2]['DeptName'], 'Asian and Middle Eastern Studies')
-                self.assertEqual(result[0]['SchoolName'], 'Non-School')
-                self.assertEqual(result[6]['SchoolCode'], 'P')
-                self.assertEqual(result[6]['SchoolName'], 'School of the Physical Sciences')
-                self.assertEqual(len(result[6]['departments']), 0)
+        result = json.loads(body_capture.value)
+        self.assertEqual(len(result), 7)
+        self.assertEqual(result[0]['SchoolCode'], 'O')
+        self.assertEqual(result[0]['SchoolName'], 'Non-School')
+        self.assertEqual(len(result[0]['departments']), 3)
+        self.assertEqual(result[0]['departments'][0]['DeptCode'], 'GHAG')
+        self.assertEqual(result[0]['departments'][0]['DeptName'], 'Anglo Saxon, Norse and Celtic')
+        self.assertEqual(result[0]['departments'][2]['DeptCode'], 'GUAG')
+        self.assertEqual(result[0]['departments'][2]['DeptName'], 'Asian and Middle Eastern Studies')
+        self.assertEqual(result[0]['SchoolName'], 'Non-School')
+        self.assertEqual(result[6]['SchoolCode'], 'P')
+        self.assertEqual(result[6]['SchoolName'], 'School of the Physical Sciences')
+        self.assertEqual(len(result[6]['departments']), 0)
